dc78fb523366f157b6f4ad3fd4fa774e629ad94e
60T-Main <guga.nadirashvili12@gmail.com>
2025-08-15 14:35:34 +0400

    first animations

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+const yesBtn = document.getElementById('yesBtn');
+const noBtn = document.getElementById('noBtn');
+
+yesBtn.addEventListener('click', () => {yesButton()});
+noBtn.addEventListener('click', () => { });
+
+
+const section1 = document.querySelector('.infoSection'),
+      section2 = document.querySelector('.infoSection2'),
+    section3 = document.querySelector('.infoSection3');
+
+
+const thanksCard = document.querySelector('.thanksCard');
+      
 function getName() {
     const params = new URLSearchParams(window.location.search);
 
     if (params.has("name")) {
-        return params.get('name');
-        
-}
+        if (params.get("name") == "") {
+            return "stumaro"
+        } else {
+            return params.get("name")
+        }
+    } else {
+        return "stumaro"
+        }; 
     
 } 
 
@@ -12,6 +31,28 @@ function showName(guestName) {
     document.getElementById('guestName').innerHTML = `Zvirfaso ${guestName}`
 }
 
+
+function yesButton() {
+    const sections = [section1, section2, section3];
+
+
+
+    thanksCard.style.setProperty("display", "flex", "important");
+    thanksCard.classList.add('animate');
+
+    setTimeout(() => {
+            sections.forEach(section => {
+        section.style.setProperty("visibility", "visible", "important");
+        section.style.setProperty("display", "flex", "important");
+        section.style.setProperty("display", "flex", "important");
+    });
+      section1.scrollIntoView({
+        behavior: "smooth"
+        });
+}, 3000); // time in milliseconds
+
+
+}
 
 
 const guestName = getName()
